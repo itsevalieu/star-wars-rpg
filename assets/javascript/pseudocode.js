05aedb00914085c6fb91dcfc8ba73a07e91a9963
Eva Lieu <itsevalieu@gmail.com>
2016-10-18 00:35:42 -0700

    Modified index and wrote priliminary pseudo code for js

---
+++ assets/javascript/pseudocode.js
@@ -0,0 +1,43 @@
+/*Steps
+
+1. Allow user to click on the img of a character
+	-moves character into "Your Character" heading
+	-enemy charas moved to "Your Enemies to Attack heading"
+2. Allow user to click on an enemy in the enemy section
+	-If statement, must choose from enemy box
+	-Selected enemy moved to Defender Section
+3. Player allowed to click attack button
+	-(IF)must meet the conditions of their character and enemy being chosen
+	-If clicked attack enemy:
+		-Enemy loses HP
+			-Represented HP under enemy shown changes
+			*need function 
+		-text shows how much HP dmg made by your character to enemy
+
+		-Enemy attacks back:
+			-your chara HP loses some
+			-rep HP under chara changes
+			*need function
+			-show text of how much HP dmg received
+	-this goes on until HP =0
+		-IF HP of enemy == 0: then remove enemy pic from defender area
+		-Player can choose new opponent
+-----------------------
+Character Attributes:
+1.Health Points = change according to atk dmg
+2.Attack Power
+	new base attack power = current base attack power + base attack power
+		-each time they attack
+
+3.Counter Attack Power
+	never changes, set amt
+-----------------------
+Characters:
+
+A] Name: 
+
+B]
+
+C]
+
+D]
